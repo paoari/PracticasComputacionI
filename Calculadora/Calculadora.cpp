1157ced8d0b5b73f54cc3c66c2cc4c7cae0a7493
Ana Arias <78131950+paoari@users.noreply.github.com>
2021-09-16 21:23:04 -0500

    Add files via upload

---
+++ Calculadora/Calculadora.cpp
@@ -0,0 +1,59 @@
+/* Bienvenides a la mejor calculadora del mundo.
+ * Creada por: Paola Arias Martínez
+ * 15 de septiembre de 2020
+ * como se quitan los comentarios
+ * ah ya
+ */
+
+using namespace std;
+#include <iostream>
+#include <math.h>
+
+int main() {
+    //Primero vamos a declarar las variables a utilizar; tenemos 1 para almacenar el caracter de la operacion a realizar
+    // y 2 para los numeros a introducir.
+
+    char op{1};
+    float num1{0}, num2{0};
+    float div;
+    double result;
+
+
+    //Vamos a escribir el comando para que el usuario indique las operaciones a realizar.
+
+    cout<< "Este programa hace operaciones basicas entre dos numeros."<<endl;
+    cout<< "Primer numero:"<<endl;
+    cin>> num1;
+    cout << "Operacion:" << endl;
+    cin >> op;
+    cout<< "Segundo numero:"<<endl;
+    cin>> num2;
+
+
+    //Ahora vamos a establecer los condicionales.
+    if (op == '+')
+    {
+        result = num1 + num2;
+    }
+    else if (op == '-')
+    {
+        result = num1 - num2;
+    }
+    else if (op == '*')
+    {
+        result = num1 * num2;
+    }
+    else if ( op == '/')
+    {
+        result = num1/num2;
+    }
+    else if (op == '%')
+    {
+        result = fmod(num1,num2);
+    }
+
+    cout << "Resultado: " <<endl;
+    cout << num1 << op << num2 << "=" << result <<endl;
+
+    return 0;
+}
