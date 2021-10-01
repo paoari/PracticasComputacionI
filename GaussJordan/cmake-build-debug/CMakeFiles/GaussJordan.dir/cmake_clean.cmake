7c849c6027f4effe35964c13a4e436c63c5a07cb
Ana Arias <78131950+paoari@users.noreply.github.com>
2021-09-30 23:05:13 -0500

    Add files via upload

---
+++ GaussJordan/cmake-build-debug/CMakeFiles/GaussJordan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaussJordan.dir/main.cpp.obj"
+  "GaussJordan.exe"
+  "GaussJordan.exe.manifest"
+  "GaussJordan.pdb"
+  "libGaussJordan.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaussJordan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
